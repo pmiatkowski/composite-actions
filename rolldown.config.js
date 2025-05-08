ec253151239b040a8f5d33dee4ca4fab240a32fe
Pawel Miatkowski <pawelmiatkowski@gmail.com>
2025-05-08 16:11:14 +0200

    chore: cleanup

---
+++ rolldown.config.js
@@ -0,0 +1,22 @@
+import {defineConfig} from "rolldown";
+
+export default defineConfig([
+  {
+    input: "actions/ai-reviewer/src/use-ai-review.js",
+    output: {
+      file: "dist/use-ai-review.js",
+      format: "cjs",
+    },
+    external: ["@google/genai"],
+    platform: "node",
+  },
+  {
+    input: "actions/ai-jsdoc/src/use-ai-jsdoc.js",
+    output: {
+      file: "dist/use-ai-jsdoc.js",
+      format: "cjs",
+    },
+    external: ["@google/genai"],
+    platform: "node",
+  }
+]);
