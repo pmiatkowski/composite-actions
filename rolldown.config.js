d8188d26a6fe970e553d4d1e550002ac2ba1346e
Pawel Miatkowski <pawelmiatkowski@gmail.com>
2025-05-08 16:57:09 +0200

    main: test

--- rolldown.config.js
+++ rolldown.config.js
@@ -6,7 +6,7 @@ export default defineConfig([
     input: "actions/ai-reviewer/src/use-ai-review.js",
     output: {
       file: "actions/ai-reviewer/dist/use-ai-review.js",
-      format: "cjs",
+      format: "es",
       inlineDynamicImports: true,
       minify: true,
     },
